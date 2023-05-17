1cf24c86b518d5bad13e0c02054e04fd2e0fe4b3
Santiago Malo <smalo.castro@gmail.com>
2023-05-17 12:18:09 -0300

    lambda from direct link

---
+++ Lambda with DAX for Direct Link/GetLongURL&Redirect.py
@@ -0,0 +1,35 @@
+import boto3
+import json
+import amazondax
+
+dax_endpoint = 'daxs://meli.bsag9w.dax-clusters.us-east-1.amazonaws.com'
+table_name = 'meli-url-shortener'
+
+dax = amazondax.AmazonDaxClient.resource(endpoint_url=dax_endpoint)
+
+def lambda_handler(event, context):
+    # Extract the path from the event object
+    path = event['rawPath']
+    
+    # Remove the first character of the path
+    path = path[1:]
+    
+    # Retrieve the long URL from DynamoDB DAX
+    table = dax.Table(table_name)
+    response = table.get_item(Key={'shortid': path})
+    
+    # Check if the item exists in the table
+    if 'Item' in response:
+        long_url = response['Item']['longurl']
+        return {
+            'statusCode': 301,
+            'headers': {
+                'Location': long_url
+            },
+            'body': ''
+        }
+    else:
+        return {
+            'statusCode': 404,
+            'body': 'Short ID not found'
+        }
